bccf60f3f91f24cf6a6fbc61663abece1c38ba02
sakshamkadayat <sakshamkadayat@gmail.com>
2024-03-21 09:01:39 +0545

    merge main

--- Hamro_Yatra/src/views/passenger/PassengerSearch.jsx
+++ Hamro_Yatra/src/views/passenger/PassengerSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useStateContext } from "../../context/ContextProvider";
 import "../../assets/styles/passenger.css";
